93d2ab39bd7f453067803bb9f00fa710eb7d4437
lucthememe <luc@coutus.net>
2024-09-27 14:16:26 -0600

    added walls made walls spawn in hit detection

--- int_scene/sketch.js
+++ int_scene/sketch.js
@@ -1,17 +1,34 @@
 // int scene
 
 
-let charX = 350;
-let charY = 400;
+let charX;
+let charY;
 let speed = 2;
+
 let wallLeftX = 0;
 let wallLeftY = 0;
 let wallLeftH = 500;
-let wallLeftW = 100;
-let wallRightX = 400;
+let wallLeftW = 50;
+
+let wallRightX = 450;
 let wallRightY = 0;
-let wallRightW = 100;
+let wallRightW = 50;
 let wallRightH = 500;
+
+let wallBottomX = 50;
+let wallBottomY = 480;
+let wallBottomW = 400;
+let wallBottomH = 20;
+
+let wallTopX = 50;
+let wallTopY = 0;
+let wallTopW = 400;
+let wallTopH = 20;
+
+let wall1X = 100;
+let wall1Y = 400;
+let wall1W = 350;
+let wall1H = 20;
 
 
 function setup() {
@@ -26,13 +43,14 @@ function draw() {
 }
 
 function isTouchingWall(wallX, wallY, wallW, wallH){
-  if(charX > wallX && charX < wallX + wallW && charY > wallY && charY < wallY + wallH){
-    charX = 350;
-    charY = 400;
+  rect(wallX, wallY, wallW, wallH);
+  if(charX+10 > wallX && charX < wallX + wallW && charY > wallY && charY < wallY + wallH){
+    charSpawn;
   }
 }
 
 function charmove(){
+  charSpawn;
   fill(100, 255 ,100);
   rect(charX, charY, 10, 10);
   if (keyIsDown(87)){
@@ -51,11 +69,19 @@ function charmove(){
 
 function drawmaze(){
   fill(255, 100, 100);
-  rect(wallLeftX, wallLeftY, wallLeftW, wallLeftH);
-  rect(wallRightX, wallRightY, wallRightW, wallRightH);
+  strokeWeight(0);
 }
 
 function hitDitection(){
-  isTouchingWall(wallLeftX, wallLeftY, wallLeftW, wallLeftH)
-  isTouchingWall(wallRightX, wallRightY, wallRightW, wallRightH)
+  fill(255, 100, 100);
+  isTouchingWall(wallLeftX, wallLeftY, wallLeftW, wallLeftH);
+  isTouchingWall(wallRightX, wallRightY, wallRightW, wallRightH);
+  isTouchingWall(wallBottomX, wallBottomY, wallBottomW, wallBottomH);
+  isTouchingWall(wallTopX, wallTopY, wallTopW, wallTopH);
+  isTouchingWall(wall1X, wall1Y, wall1W, wall1H);
+}
+
+function charSpawn(){
+  charX = 400;
+  charY = 400;
 }
