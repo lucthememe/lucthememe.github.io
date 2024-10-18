577d92d4e2f0171286d1554ca7f50940e9d15206
lucthememe <luc@coutus.net>
2024-10-18 14:16:41 -0600

    added movement to the beam

--- terra/sketch.js
+++ terra/sketch.js
@@ -2,17 +2,22 @@
 // 24/10/7
 
 let terrain = [];
-const NUMBER_OF_RECTS = 5000;
 let spawn_height = 0;
+const NUMBER_OF_RECTS = 2500;
 let char;
+let death;
+let move_beam = 0;
 
+function preload(){
+  death = loadSound("dead.ogg");
+
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let how_wide = width / NUMBER_OF_RECTS;
   genrateTerrain(how_wide);
   spawn_char(spawn_height + 50);
-
 }
 
 function draw() {
@@ -20,13 +25,14 @@ function draw() {
   spawn_beam();
   draw_char();
   move_char();
+  move_beam += 5;
 }
 
 function genrateTerrain(theWidth){
   let counter = 0;
   let time = 0;
-  let timeDelta = 0.0002;
-  for (let x = 0; x < width; x += theWidth){
+  let timeDelta = 0.0004;
+  for (let x = 0; x < width * 3; x += theWidth){
     let theHeight = noise(time) * (height *0.8);
     counter++;
     if (counter === 1){
@@ -37,6 +43,7 @@ function genrateTerrain(theWidth){
     time += timeDelta;
   }
 }
+
 
 function spawn_rect(leftSide, rectHeight, rectWidth){
   let theRect = {
@@ -52,16 +59,16 @@ function spawn_beam(){
   noStroke();
   for (let someRect of terrain){
     fill(150, 0, 250);
-    rect(someRect.x, someRect.y, someRect.w, someRect.h);
+    rect(someRect.x - move_beam, someRect.y, someRect.w, someRect.h);
     fill(150, 0, 200);
-    rect(someRect.x, someRect.y - 10, someRect.w, someRect.h + 20);
+    rect(someRect.x - move_beam, someRect.y - 10, someRect.w, someRect.h + 20);
   }
 }
 
 function in_beam(){
   for (let somerect in terrain){
-    if (char.x > somerect.x && char.x < somerect.x + 10 && char.y > somerect.y && char.y < somerect.y + 100){
-
+    if (char.y > somerect.y && char.y < somerect.y + 100){
+      death.play();
 
     }
   }
@@ -71,7 +78,7 @@ function spawn_char(Y){
   let the_char = {
     y: Y,
     x: 0,
-    speed: 20,
+    speed: 5,
     size: 20,
   };
   char = the_char;
@@ -80,16 +87,10 @@ function spawn_char(Y){
 
 function move_char(){
   if (keyIsDown(87)){
-    char.y = char.y + 10;
+    char.y = char.y - char.speed;
   }
   if (keyIsDown(83)){
-    char.y = char.y + 10;
-  }
-  if (keyIsDown(65)){
-    char.x = char.x + 10;
-  }
-  if (keyIsDown(68)){
-    char.x = char.x + 10;
+    char.y = char.y + char.speed;
   }
 }
 
