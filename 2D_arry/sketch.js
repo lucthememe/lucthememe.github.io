2e51cdb13afc3a34639a2629465432bb8549c0f8
lucthememe <luc@coutus.net>
2024-11-15 14:09:14 -0600

    balanced dirt and grass commented code and fixed player img

--- 2D_arry/sketch.js
+++ 2D_arry/sketch.js
@@ -14,9 +14,13 @@ const stone_wall_width = 5;
 const number_of_tiles = 11;
 const cam_offset = 4;
 const fuelPrice = 5;
+const playerStartX = 5;
+const playerStarty = 2;
 let tile_size = 0;
 let grid_map;
 
+
+//setting variables for the player
 let player ={
   y: -2,
   x: -5,
@@ -32,19 +36,16 @@ let player ={
 
 // diffineing objects
 let mineables ={
-  grass: [1, 0],
-  earth: [2, 0],
-  stone: [3, 0],
+  grass: [1, -5],
+  earth: [2, -5],
+  stone: [3, -5],
   iron: [4, 30],
   copper: [5, 60],
   gold: [6, 250],
-  emrald: [7, 2500],
-  treasure: [8,5000],
+  emrald: [7, 500],
+  treasure: [8,1000],
 };
 
-let refuel_station;
-let ore_shop;
-let upgrade_shop;
 
 // setting up variables for img's
 let images ={
@@ -62,6 +63,9 @@ let images ={
   char_right: null,
 };
 
+/**
+ * preloads images
+ */
 function preload(){
   images.earth_img = loadImage("earth.png");
   images.stone_img = loadImage("rock.png");
@@ -78,18 +82,22 @@ function preload(){
 }
 
 function setup() {
+  alert("mine with W A S D or arrow keys. watch your fuel and inventory if you run out of fuel or fill your invintory you lose. you can sell your mineables and refule at the surface. be carfull mineables sell but dirt and grass cost you. fule costs $5 per unit.");
+  //setting tile size
   tile_size = windowWidth/number_of_tiles;
   createCanvas(windowWidth, windowHeight);
+  //genrateing map
   grid_map = gen_grid(MAP_WIDTH, MAP_HIGHT, grass_lvl, air_height, stone_wall_width);
 }
 
 function draw() {
+  //checking if the player has fuel
   if (fuelCheck(player.fuel) === true){
-  background(1, 51, 1);
-  sellAndRefuel(player.y, fuelPrice, player.fuelSize)
-  draw_grid(grid_map, tile_size, player.cam_y, player.cam_x);
-  displayPlayerStats(player.bank, player.fuel, player.fuelSize, player.inventory.length, player.inventorySize);
-  displayPlayer(player.x*-1, player.y*-1, player.lastMove, tile_size);
+    background(1, 51, 1);
+    sellAndRefuel(player.y, fuelPrice, player.fuelSize);
+    draw_grid(grid_map, tile_size, player.cam_y, player.cam_x);
+    displayPlayerStats(player.bank, player.fuel, player.fuelSize, player.inventory.length, player.inventorySize);
+    displayPlayer(playerStartX, playerStarty, player.lastMove, tile_size);
   }
 }
 
@@ -106,10 +114,12 @@ function gen_grid(coloms, rows, grass, air, stone_wall){
   let new_grid = [];
   let row_temp = [];
 
+  //adds air layers
   for (let y = 0; y <= air; y++){
     new_grid.push(row_temp);
   }
 
+  //adds stone wall
   row_temp = [];
   for (let rock = 0; rock <= stone_wall-1; rock++){
     row_temp.push(mineables.stone);
@@ -123,31 +133,35 @@ function gen_grid(coloms, rows, grass, air, stone_wall){
   new_grid.push(row_temp);
   row_temp = [];
 
+  //adds grass
   for (let y = grass+1; y <= rows; y++){
     for (let rock = 0; rock <= stone_wall-1; rock++){
       row_temp.push(mineables.stone);
     }
+
+    //makes rest of grid
     for (let x = 5; x <= coloms - 5; x++){
       let funny_little_guy = random(1000);
-      if (funny_little_guy <= 500){
+      if (funny_little_guy <= 750){
         row_temp.push(mineables.earth);
       }
-      else if (funny_little_guy > 500 && funny_little_guy <= 700){
+      else if (funny_little_guy > 750 && funny_little_guy <= 900){
         row_temp.push(mineables.iron);
       }
-      else if (funny_little_guy > 700 && funny_little_guy <= 800){
+      else if (funny_little_guy > 900 && funny_little_guy <= 980){
         row_temp.push(mineables.copper);
       }
-      else if (funny_little_guy > 800 && funny_little_guy <= 975){
+      else if (funny_little_guy > 980 && funny_little_guy <= 995){
         row_temp.push(mineables.gold);
       }
-      else if (funny_little_guy > 975 && funny_little_guy <= 990){
+      else if (funny_little_guy > 995 && funny_little_guy <= 999){
         row_temp.push(mineables.emrald);
       }
-      else if (funny_little_guy > 990 && funny_little_guy <= 1000){
+      else if (funny_little_guy > 999 && funny_little_guy <= 1000){
         row_temp.push(mineables.treasure);
       }
     }
+    //adds stone wall
     for (let rock = 0; rock <= stone_wall-1; rock++){
       row_temp.push(mineables.stone);
     }
@@ -157,7 +171,13 @@ function gen_grid(coloms, rows, grass, air, stone_wall){
   return new_grid;
 }
 
-
+/**
+ * draws the grid thats imputed
+ * @param {*grid the grid you want drawn} 
+ * @param {*square_size the size of each square} 
+ * @param {*cam_y the players camra's y} 
+ * @param {*cam_x the players camra's x} 
+ */
 function draw_grid(grid, square_size, cam_y, cam_x){
   for (let y = 0; y < MAP_HIGHT; y++){
     for (let x = 0; x < MAP_WIDTH+1; x++){
@@ -189,66 +209,94 @@ function draw_grid(grid, square_size, cam_y, cam_x){
   }
 }
 
+/**
+ * checks if the player is trying to move
+ * checks if the player has room in the inventory
+ */
 function keyPressed(){
-  if (key === "s" || keyCode === (40)){
+  console.log(player);
+  if (key === "s" || keyCode === 40){
     if (player.cam_y >= -MAP_HIGHT + number_of_tiles+1 && inventoryCheck(player.inventory.length, player.inventorySize)){
       player.cam_y--;
       player.y--;
       player.fuel--;
       mine_tile(player.x, player.y);
-      player.lastMove = "down"
+      player.lastMove = "down";
     }
   }
-  if (key === "w" || keyCode ===(38)){
+  if (key === "w" || keyCode === 38){
     if (player.cam_y <= 0 && inventoryCheck(player.inventory.length, player.inventorySize)){
       player.cam_y++;
       player.y++;
       player.fuel--;
       mine_tile(player.x, player.y);
-      player.lastMove = "up"
+      player.lastMove = "up";
     }
   }
-  if (key === "d" || keyCode ===(39)){
+  if (key === "d" || keyCode === 39){
     if (player.cam_x >= -MAP_WIDTH + number_of_tiles && inventoryCheck(player.inventory.length, player.inventorySize)){
       player.cam_x--;
       player.x--;
       player.fuel--;
       mine_tile(player.x, player.y);
-      player.lastMove = "right"
+      player.lastMove = "right";
     }
   }
-  if (key === "a"  || keyCode ===(37)){
+  if (key === "a"  || keyCode === 37){
     if (player.cam_x <= -1 && inventoryCheck(player.inventory.length, player.inventorySize)){
       player.cam_x++;
       player.x++;
       player.fuel--;
       mine_tile(player.x, player.y);
-      player.lastMove = "left"
+      player.lastMove = "left";
     }
   }
+  console.log(player);
 }
 
+/**
+ * takes the tile the player moves to and deletes it and logs the item it mined
+ * @param {*next_tile_x the x coord the player moves into} 
+ * @param {*next_tile_y the x coord the player moves into} 
+ */
 function mine_tile(next_tile_x, next_tile_y){
-  next_tile_x = next_tile_x*-1
-  next_tile_y = next_tile_y*-1
+  next_tile_x = next_tile_x*-1;
+  next_tile_y = next_tile_y*-1;
   if (grid_map[next_tile_y][next_tile_x] !== 0){
     player.inventory.push(grid_map[next_tile_y][next_tile_x][1]);
     grid_map[next_tile_y][next_tile_x] = 0;
   }
 }
 
+/**
+ * checks fuel lvls
+ * @param {*fuel the players curant fuel}  
+ * @returns true of false for if the player has fuel
+ */
 function fuelCheck(fuel){
-  return(fuel > 0);
+  return fuel > 0;
 }
 
+/**
+ * checks inventory lvls
+ * @param {*inventory the players }  
+ * @param {*inventorySize size of the players inventory}
+ * @returns true of false for if the player has inventory space
+ */
 function inventoryCheck(inventory, inventorySize){
-  return(inventory < inventorySize);
+  return inventory < inventorySize;
 }
 
+/**
+ * sells mineables and refules if the player is on the surface
+ * @param {*y the y crood of the player} 
+ * @param {*price the price of fuel} 
+ * @param {*fuelSize the size of the fuel tank} 
+ */
 function sellAndRefuel(y, price, fuelSize){
   if (y === -3){
     for (let i = player.inventory.length; i > 0; i--){
-      player.bank += player.inventory[i-1]
+      player.bank += player.inventory[i-1];
       player.inventory.pop();
     }
     if (player.fuel < fuelSize && player.bank >= price){
@@ -258,29 +306,44 @@ function sellAndRefuel(y, price, fuelSize){
   }
 }
 
+/**
+ * displayes the players bank fuel and inventory 
+ * @param {*bank the players curant bank balance} 
+ * @param {*fuel the players curant fuel}  
+ * @param {*fuelSize the size of the fuel tank} 
+ * @param {*inventory the players }  
+ * @param {*inventorySize size of the players inventory}
+ */
 function displayPlayerStats(bank, fuel, fuelSize, inventory, inventorySize){
   fill("black");
   textSize(20);
   text("Bank: $" + bank, 20, 20);
   text("Fuel: " + fuel + "/" + fuelSize, 20, 40);
-  text("Inventory: " + inventory + "/" + inventorySize, 20, 60)
+  text("Inventory: " + inventory + "/" + inventorySize, 20, 60);
 }
 
+/**
+ * diplays the player
+ * @param {*x the x crood of the player}
+ * @param {*y the y crood of the player}
+ * @param {*direction the last direction the player moved} 
+ * @param {*square_size the size of the square} 
+ */
 function displayPlayer(x, y, direction, square_size){
-  let char = images.char_right
+  let char = images.char_right;
 
   if (direction === "up"){
-    char = images.char_up
+    char = images.char_up;
   }
   else if (direction === "down"){
-    char = images.char_down
+    char = images.char_down;
   }
   else if (direction === "left"){
-    char = images.char_left
+    char = images.char_left;
   }
   else if (direction === "right"){
-    char = images.char_right
+    char = images.char_right;
   }
 
-  image(char, (x)*square_size, (y)*square_size, square_size, square_size);
+  image(char, x*square_size, y*square_size, square_size, square_size);
 }
